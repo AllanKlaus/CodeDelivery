13ffe9fc34ea7f91fd41cc3b0b1534a496e4b3cb
Allan <allab_klaus@globo.com>
2015-10-05 11:52:39 -0300

    correção tabela order_items

--- database/migrations/2015_09_15_161911_create_orders_table_itens.php
+++ database/migrations/2015_09_15_161911_create_orders_table_itens.php
@@ -12,7 +12,7 @@ class CreateOrdersTableItens extends Migration
      */
     public function up()
     {
-        Schema::create('orders_itens', function (Blueprint $table) {
+        Schema::create('order_items', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('product_id')->unsigned();
             $table->foreign('product_id')->references('id')->on('products');
@@ -35,6 +35,6 @@ class CreateOrdersTableItens extends Migration
      */
     public function down()
     {
-        Schema::drop('orders_itens');
+        Schema::drop('order_items');
     }
 }
