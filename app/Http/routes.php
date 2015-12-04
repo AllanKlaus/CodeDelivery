508d3b658ed15ef1ed2ba6f9b8e2ac83723b68d0
Allan <allab_klaus@globo.com>
2015-12-04 00:12:50 -0200

    Finalizada parte 6 e parte 7

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -91,5 +91,31 @@ Route::group(['prefix'=>'customer', 'middleware' => 'auth.checkrole:client', 'as
         Route::get('create', ['as' => 'create', 'uses' => 'CheckoutController@create']);
         Route::post('store', ['as' => 'store', 'uses' => 'CheckoutController@store']);
     });
+});
 
+Route::post('oauth/access_token', function() {
+    return Response::json(Authorizer::issueAccessToken());
+});
+
+Route::group(['prefix'=>'api', 'middleware' => 'oauth', 'as' => 'api.'], function(){
+
+    Route::group(['prefix'=>'client', 'middleware' => 'oauth.checkrole:client', 'as' => 'client.'], function(){
+        Route::get('pedidos', function(){
+            return [
+                'id' => 1,
+                'name' => 'Allan Klaus - CLient',
+                'total' => '10'
+            ];
+        });
+    });
+
+    Route::group(['prefix'=>'deliveryman', 'middleware' => 'oauth.checkrole:deliveryman', 'as' => 'deliveryman.'], function(){
+        Route::get('pedidos', function(){
+            return [
+                'id' => 1,
+                'name' => 'Allan Klaus - Entregador',
+                'total' => '10'
+            ];
+        });
+    });
 });
